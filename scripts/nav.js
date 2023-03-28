01b99d2b259de0bc904817496ed426baaa621453
Jenny Su <71398958+jennysu1105@users.noreply.github.com>
2023-03-28 19:09:04 -0400

    project details

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,9 +1,9 @@
-class Navigation extends HTMLElement{
-    constructor(){
+class Navigation extends HTMLElement {
+    constructor() {
         super();
     }
 
-    connectCallback(){
+    connectedCallback() {
         this.innerHTML = `
         <nav class="navbar navbar-extend-lg navbar-light bg-light">
             <div class="container-fluid">
@@ -18,9 +18,13 @@ class Navigation extends HTMLElement{
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="index.php">Project Details</a>
+                            <a class="nav-link" href="index.html">Project Details</a>
                         </li>
                         <li class="nav-item">
+                            <a class="nav-link" href="/music_list_project/tasks.html">Task List</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/music_list_project/resources.html">Resources</a>
                         </li>
                     </ul>
                 </div>
